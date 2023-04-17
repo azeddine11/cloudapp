52cb99b632bda17f541f3ee710859e0617f819c7
azeddine11 <azeddineittaakat1r@gmail.com>
2023-04-17 21:44:06 +0000

    new update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import HomeSection from './components/HomeSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
-import Map from './components/Map';
+import './App.css';  // Importing CSS file for styling
+
+import Navbar from './components/Navbar';  // Importing Navbar component
+import HomeSection from './components/HomeSection';  // Importing HomeSection component
+import ContactSection from './components/ContactSection';  // Importing ContactSection component
+import Footer from './components/Footer';  // Importing Footer component
+import Map from './components/Map';  // Importing Map component
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <HomeSection />
-      <ContactSection />
-      <Map/>
+      <Navbar/>  {/* Rendering Navbar component */}
+      <HomeSection />  {/* Rendering HomeSection component */}
+      <ContactSection />  {/* Rendering ContactSection component */}
+      <Map/>  {/* Rendering Map component */}
       
-   
+      {/* 
+      <Footer />  // Footer component is not being used in the code
+      */}
     </div>
   );
 }
 
-export default App;
+export default App;  // Exporting App component as the default export
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,40 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Assuming you're using react-router for routing
-import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
+import { Link } from "react-router-dom"; // Import Link from react-router for routing
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter from react-router
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // Use useState hook to manage state for isOpen
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(!isOpen); // Toggle isOpen state when button is clicked
   };
 
   return (
     <Router> {/* Wrap the Navbar component with BrowserRouter */}
       <nav className="navbar">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo"> {/* Create a link to the home page */}
           Logo
         </Link>
-        <div className={isOpen ? "navbar-menu open" : "navbar-menu"}>
+        <div className={isOpen ? "navbar-menu open" : "navbar-menu"}> {/* Use isOpen state to conditionally apply CSS class for menu */}
           <ul className="navbar-menu-list">
             <li className="navbar-menu-item">
-              <Link to="/home" className="navbar-menu-link">
+              <Link to="/home" className="navbar-menu-link"> {/* Create a link to the home page */}
                 Home
               </Link>
             </li>
             <li className="navbar-menu-item">
-              <Link to="/about" className="navbar-menu-link">
+              <Link to="/about" className="navbar-menu-link"> {/* Create a link to the about page */}
                 About
               </Link>
             </li>
             <li className="navbar-menu-item">
-              <Link to="/contact" className="navbar-menu-link">
+              <Link to="/contact" className="navbar-menu-link"> {/* Create a link to the contact page */}
                 Contact
               </Link>
             </li>
           </ul>
         </div>
-        <button className="navbar-toggle" onClick={toggleMenu}>
+        <button className="navbar-toggle" onClick={toggleMenu}> {/* Create a button to toggle the menu */}
           {isOpen ? (
             <i className="fas fa-times"></i>
           ) : (
